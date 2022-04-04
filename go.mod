4bd999179bae294c02208ac51b6a16eea4d777f7
Mattias Ahlstedt <mattias.a@luda.farm>
2022-04-04 10:30:05 +0200

    Added stripe event router

--- go.mod
+++ go.mod
@@ -1,3 +1,26 @@
 module github.com/luda-farm/libs
 
 go 1.18
+
+require (
+	cloud.google.com/go/cloudtasks v1.3.0
+	github.com/stripe/stripe-go/v72 v72.99.0
+	google.golang.org/genproto v0.0.0-20220401170504-314d38edb7de
+)
+
+require (
+	cloud.google.com/go/compute v1.3.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.70.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ router/stripe.go
@@ -0,0 +1,72 @@
+package router
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
+	"github.com/stripe/stripe-go/v72/webhook"
+	"google.golang.org/genproto/googleapis/cloud/tasks/v2"
+)
+
+type (
+	StripeEventRouter struct {
+		router *Router
+	}
+	StripeEventRouterConfig struct {
+		host, gcpProject, gcpLocation, stripeWebhookSecret string
+	}
+)
+
+func NewStripeEventRouter(router *Router, config StripeEventRouterConfig) StripeEventRouter {
+	client, err := cloudtasks.NewClient(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	router.Post("/stripe/events", func(ctx Context) {
+		payload := ctx.RawRequestBody()
+		signature := ctx.Request.Header.Get("Stripe-Signature")
+		event, err := webhook.ConstructEvent(payload, signature, config.stripeWebhookSecret)
+		if err != nil {
+			ctx.Response.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		task := tasks.CreateTaskRequest{
+			Parent: fmt.Sprintf(
+				"projects/%s/locations/%s/queues/stripe-events",
+				config.gcpProject, config.gcpLocation,
+			),
+			Task: &tasks.Task{
+				MessageType: &tasks.Task_HttpRequest{
+					HttpRequest: &tasks.HttpRequest{
+						HttpMethod: tasks.HttpMethod_POST,
+						Url:        config.host + pathFromEvent(event.Type),
+						Body:       event.Data.Raw,
+					},
+				},
+			},
+		}
+
+		if _, err := client.CreateTask(context.Background(), &task); err != nil {
+			panic(err)
+		}
+
+		ctx.Response.WriteHeader(http.StatusNoContent)
+	})
+
+	return StripeEventRouter{
+		router: router,
+	}
+}
+
+func pathFromEvent(event string) string {
+	return "/stripe/" + strings.Join(strings.Split(event, "."), "/")
+}
+
+func (router StripeEventRouter) Handle(event string, handler Handler) {
+	router.router.Post(pathFromEvent(event), handler)
+}
