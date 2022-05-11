ccdbc3bb7f4b3175a5a271b299462b39b24ad158
Mattias Ahlstedt <mattias.a@luda.farm>
2022-05-11 13:38:11 +0200

    Refactored back to std http pkg

--- router/router.go
+++ router/router.go
@@ -1,154 +1,88 @@
 package router
 
 import (
-	"fmt"
-	"math/rand"
+	"log"
 	"net/http"
 	"regexp"
 	"runtime/debug"
 	"strings"
-	"time"
-)
 
-const (
-	Retry = -1
+	"golang.org/x/exp/maps"
 )
 
 type (
-	// Return status < 100 to trigger a retry
-	Handler func(ctx *Context) int
-
 	Router struct {
 		allowedOrigins []string
-		handlers       map[string]*handlerGroup
+		handlers       map[string]map[string]Handler
 	}
 
-	handlerGroup struct {
-		delete, get, post, put Handler
-	}
-)
-
-var (
-	MaxRetries int
+	Handler func(res http.ResponseWriter, req *http.Request, params map[string]string)
 )
 
 func New() Router {
-	return Router{handlers: make(map[string]*handlerGroup)}
+	return Router{handlers: map[string]map[string]Handler{}}
 }
 
 func (router *Router) AllowOrigin(origin string) {
 	router.allowedOrigins = append(router.allowedOrigins, origin)
 }
 
-func (router *Router) Delete(pattern string, handler Handler) {
-	if handlers, ok := router.handlers[pattern]; ok {
-		handlers.delete = handler
-	} else {
-		router.handlers[pattern] = &handlerGroup{delete: handler}
+func (router *Router) Handle(method, path string, handler Handler) {
+	_, ok := router.handlers[path]
+	if !ok {
+		router.handlers[path] = map[string]Handler{
+			http.MethodOptions: func(res http.ResponseWriter, req *http.Request, _ map[string]string) {
+				res.WriteHeader(http.StatusNoContent)
+			},
+		}
 	}
-}
-
-func (router *Router) Get(pattern string, handler Handler) {
-	if group, ok := router.handlers[pattern]; ok {
-		group.get = handler
-	} else {
-		router.handlers[pattern] = &handlerGroup{get: handler}
-	}
-}
-
-func (router *Router) Post(pattern string, handler Handler) {
-	if handlers, ok := router.handlers[pattern]; ok {
-		handlers.post = handler
-	} else {
-		router.handlers[pattern] = &handlerGroup{post: handler}
-	}
-}
-
-func (router *Router) Put(pattern string, handler Handler) {
-	if handlers, ok := router.handlers[pattern]; ok {
-		handlers.put = handler
-	} else {
-		router.handlers[pattern] = &handlerGroup{put: handler}
-	}
+	router.handlers[path][method] = handler
 }
 
 func (router Router) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	defer panicRecovery(res)
-	for pattern, group := range router.handlers {
-		matches, params := matchPattern(pattern, req.URL.Path)
+	defer func() {
+		switch err := recover().(type) {
+		case nil:
+			return
+		case error:
+			log.Printf("500 Internal Server Error: %s\n%s", err.Error(), filteredStackTrace())
+		default:
+			log.Printf("500 Internal Server Error: %v\n%s", err, filteredStackTrace())
+		}
+		res.WriteHeader(http.StatusInternalServerError)
+	}()
+
+	for path, methods := range router.handlers {
+		matches, params := matchPath(path, req.URL.Path)
 		if !matches {
 			continue
 		}
 
-		ctx := Context{
-			Request:  req,
-			response: res,
-			Params:   params,
-		}
-
 		// Handle CORS
 		res.Header().Set("vary", "origin")
-		res.Header().Set("access-control-allow-methods", group.allowedMethods())
 		res.Header().Add("access-control-allow-headers", "authorization")
 		res.Header().Add("access-control-allow-headers", "content-type")
+		for _, method := range maps.Keys(methods) {
+			res.Header().Add("access-control-allow-methods", method)
+		}
 		for _, origin := range router.allowedOrigins {
-			if origin == "*" || origin == ctx.Request.Header.Get("origin") {
+			if origin == "*" || origin == req.Header.Get("origin") {
 				res.Header().Set("access-control-allow-origin", origin)
 				break
 			}
 		}
 
-		if req.Method == http.MethodOptions {
-			res.WriteHeader(http.StatusNoContent)
-			return
-		}
-
-		handler, ok := group.handler(req.Method)
+		handler, ok := methods[req.Method]
 		if !ok {
 			break
 		}
 
-		for i := 0; i <= MaxRetries; i++ {
-			status := handler(&ctx)
-			if status == Retry {
-				// random back off up to 100 ms
-				time.Sleep(time.Duration(rand.Intn(1e8)))
-				continue
-			}
-
-			res.WriteHeader(status)
-			if status != http.StatusNoContent {
-				res.Write(ctx.ResponseBody)
-			}
-			return
-		}
-	}
-	res.WriteHeader(http.StatusNotFound)
-}
-
-func panicRecovery(res http.ResponseWriter) {
-	err := recover()
-	if err == nil {
+		handler(res, req, params)
 		return
 	}
-
-	stackTrace := make([]string, 0)
-	for _, line := range strings.Split(string(debug.Stack()), "\n") {
-		if regexp.MustCompile(`\.go:[1-9][0-9]*`).MatchString(line) {
-			stackTrace = append(stackTrace, line, "\n")
-		}
-	}
-
-	switch err := err.(type) {
-	case error:
-		fmt.Println("[PANIC]", err.Error(), "\n", stackTrace)
-	default:
-		fmt.Println("[PANIC]", err, "\n", stackTrace)
-	}
-	res.WriteHeader(http.StatusInternalServerError)
 }
 
-func matchPattern(rawPattern, rawRequest string) (bool, map[string]string) {
+func matchPath(rawPattern, rawRequest string) (bool, map[string]string) {
 	pattern, request := strings.Split(rawPattern, "/"), strings.Split(rawRequest, "/")
 	if len(pattern) != len(request) {
 		return false, nil
@@ -169,34 +103,14 @@ func matchPattern(rawPattern, rawRequest string) (bool, map[string]string) {
 	return true, params
 }
 
-func (group handlerGroup) handler(method string) (Handler, bool) {
-	switch method {
-	case http.MethodDelete:
-		return group.delete, group.delete != nil
-	case http.MethodGet:
-		return group.get, group.get != nil
-	case http.MethodPost:
-		return group.post, group.post != nil
-	case http.MethodPut:
-		return group.put, group.put != nil
-	default:
-		return nil, false
+func filteredStackTrace() string {
+	srcCodeLine := regexp.MustCompile(`((cmd)|(internal))/.+\.go`)
+	buffer := strings.Builder{}
+	for _, line := range strings.Split(string(debug.Stack()), "\n") {
+		if srcCodeLine.MatchString(line) {
+			buffer.WriteString(line)
+			buffer.WriteRune('\n')
+		}
 	}
-}
-
-func (group handlerGroup) allowedMethods() string {
-	methods := make([]string, 0)
-	if group.delete != nil {
-		methods = append(methods, http.MethodDelete)
-	}
-	if group.get != nil {
-		methods = append(methods, http.MethodGet)
-	}
-	if group.post != nil {
-		methods = append(methods, http.MethodPost)
-	}
-	if group.put != nil {
-		methods = append(methods, http.MethodPut)
-	}
-	return strings.Join(methods, ", ")
+	return buffer.String()
 }
--- router/stripe.go
+++ router/stripe.go
@@ -3,11 +3,11 @@ package router
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 
 	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
-	"github.com/luda-farm/libs/assert"
 	"github.com/stripe/stripe-go/v72/webhook"
 	"google.golang.org/genproto/googleapis/cloud/tasks/v2"
 )
@@ -18,39 +18,56 @@ type (
 	}
 )
 
-func (router *Router) InitStripeEventHandling(config StripeEventHandlingConfig) {
-	client := assert.MustChain(cloudtasks.NewClient(context.Background()))
+func (router *Router) InitStripeEventHandling(config StripeEventHandlingConfig) error {
+	client, err := cloudtasks.NewClient(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to connect to cloudtasks: %w", err)
+	}
 
-	router.Post("/stripe/events", func(ctx *Context) int {
-		event, err := webhook.ConstructEvent(
-			ctx.RequestBody(),
-			ctx.Request.Header.Get("stripe-signature"),
-			config.StripeWebhookSecret,
-		)
-		if err != nil {
-			return http.StatusUnauthorized
-		}
+	router.Handle(http.MethodPost, "/stripe/events",
+		func(res http.ResponseWriter, req *http.Request, _ map[string]string) {
+			requestBody, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				res.WriteHeader(http.StatusBadRequest)
+				return
+			}
 
-		url := "https://" + ctx.Request.Host + pathFromEvent(event.Type)
-		task := tasks.CreateTaskRequest{
-			Parent: fmt.Sprintf(
-				"projects/%s/locations/%s/queues/stripe-events",
-				config.GcpProject, config.GcpLocation,
-			),
-			Task: &tasks.Task{
-				MessageType: &tasks.Task_HttpRequest{
-					HttpRequest: &tasks.HttpRequest{
-						HttpMethod: tasks.HttpMethod_POST,
-						Url:        url,
-						Body:       event.Data.Raw,
+			event, err := webhook.ConstructEvent(
+				requestBody,
+				req.Header.Get("stripe-signature"),
+				config.StripeWebhookSecret,
+			)
+			if err != nil {
+				res.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+
+			url := "https://" + req.Host + pathFromEvent(event.Type)
+			task := tasks.CreateTaskRequest{
+				Parent: fmt.Sprintf(
+					"projects/%s/locations/%s/queues/stripe-events",
+					config.GcpProject, config.GcpLocation,
+				),
+				Task: &tasks.Task{
+					MessageType: &tasks.Task_HttpRequest{
+						HttpRequest: &tasks.HttpRequest{
+							HttpMethod: tasks.HttpMethod_POST,
+							Url:        url,
+							Body:       event.Data.Raw,
+						},
 					},
 				},
-			},
-		}
+			}
 
-		assert.MustChain(client.CreateTask(context.Background(), &task))
-		return http.StatusNoContent
-	})
+			_, err = client.CreateTask(context.Background(), &task)
+			if err != nil {
+				res.WriteHeader(http.StatusInternalServerError)
+			}
+
+			res.WriteHeader(http.StatusNoContent)
+		},
+	)
+	return nil
 }
 
 func pathFromEvent(event string) string {
@@ -58,5 +75,5 @@ func pathFromEvent(event string) string {
 }
 
 func (router *Router) HandleStripeEvent(event string, handler Handler) {
-	router.Post(pathFromEvent(event), handler)
+	router.Handle(http.MethodPost, pathFromEvent(event), handler)
 }
