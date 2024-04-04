c544a5d1266769ae127bb7b302afa020e46fa3bf
Mattias Ahlstedt <mattias.a@luda.farm>
2024-04-04 08:37:13 +0200

    added assert.NotNil

--- assert/assert.go
+++ assert/assert.go
@@ -1,7 +1,15 @@
 package assert
 
+import "fmt"
+
 func Nil(a any) {
 	if a != nil {
-		panic(a)
+		panic(fmt.Errorf("assert.Nil(%v)", a))
+	}
+}
+
+func NotNil(a any) {
+	if a == nil {
+		panic(fmt.Errorf("assert.NotNil(%v)", a))
 	}
 }
