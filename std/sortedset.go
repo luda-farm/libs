e5b692280d3cdd733b99ded5633ca8bfce556caa
Mattias Ahlstedt <mattias.a@luda.farm>
2025-10-29 11:04:41 +0100

    fix!: optimised sortedset implementation

--- std/sortedset.go
+++ std/sortedset.go
@@ -18,14 +18,11 @@ func (set *SortedSet[T]) Add(t T) bool {
 }
 
 // returns whether the set was modified
-func (set *SortedSet[T]) AddAll(t []T) bool {
-	modified := false
-	for _, element := range t {
-		if set.Add(element) {
-			modified = true
-		}
-	}
-	return modified
+func (set *SortedSet[T]) AddAll(slice []T) bool {
+	originalLength := len(*set)
+	other := NewSortedSetFromSlice(slice)
+	*set = set.Union(other)
+	return len(*set) > originalLength
 }
 
 // returns whether the set was modified
@@ -34,24 +31,8 @@ func (set *SortedSet[T]) Remove(t T) bool {
 	if !ok {
 		return false
 	}
-	*set = append((*set)[:i], (*set)[i+1:]...)
+	*set = slices.Delete(*set, i, i+1)
 	return true
-}
-
-func (set SortedSet[T]) Complement(other SortedSet[T]) SortedSet[T] {
-	var complement SortedSet[T]
-	for i, j := 0, 0; i < len(set) && j < len(other); {
-		if set[i] < other[j] {
-			complement = append(complement, set[i])
-			i++
-		} else if other[j] < set[i] {
-			j++
-		} else {
-			i++
-			j++
-		}
-	}
-	return complement
 }
 
 func (set SortedSet[T]) Contains(t T) bool {
@@ -59,35 +40,86 @@ func (set SortedSet[T]) Contains(t T) bool {
 	return ok
 }
 
-func (set SortedSet[T]) Intersection(other SortedSet[T]) SortedSet[T] {
-	var intersection SortedSet[T]
-	for i, j := 0, 0; i < len(set) && j < len(other); {
-		if set[i] < other[j] {
+func (set SortedSet[T]) Difference(other SortedSet[T]) SortedSet[T] {
+	difference := make(SortedSet[T], 0, len(set))
+
+	i, j := 0, 0
+	for i < len(set) && j < len(other) {
+		switch {
+		case set[i] < other[j]:
+			difference = append(difference, set[i])
 			i++
-		} else if other[j] < set[i] {
+		case other[j] < set[i]:
 			j++
-		} else {
+		default:
+			i++
+			j++
+		}
+	}
+
+	if i < len(set) {
+		difference = append(difference, set[i:]...)
+	}
+
+	return difference
+}
+
+func (set SortedSet[T]) Intersection(other SortedSet[T]) SortedSet[T] {
+	capacity := len(set)
+	if len(other) < len(set) {
+		capacity = len(other)
+	}
+
+	intersection := make(SortedSet[T], 0, capacity)
+
+	for i, j := 0, 0; i < len(set) && j < len(other); {
+		switch {
+		case set[i] < other[j]:
+			i++
+		case other[j] < set[i]:
+			j++
+		default:
 			intersection = append(intersection, set[i])
 			i++
 			j++
 		}
 	}
+
 	return intersection
 }
 
 func (set SortedSet[T]) Union(other SortedSet[T]) SortedSet[T] {
-	if len(set) < len(other) {
-		set, other = other, set
+	union := make(SortedSet[T], 0, len(set)+len(other))
+
+	i, j := 0, 0
+	for i < len(set) && j < len(other) {
+		switch {
+		case set[i] < other[j]:
+			union = append(union, set[i])
+			i++
+		case other[j] < set[i]:
+			union = append(union, other[j])
+			j++
+		default:
+			union = append(union, set[i])
+			i++
+			j++
+		}
 	}
-	union := SliceToSortedSet(set)
-	for _, t := range other {
-		union.Add(t)
+
+	switch {
+	case i < len(set):
+		union = append(union, set[i:]...)
+	case j < len(other):
+		union = append(union, other[j:]...)
 	}
+
 	return union
 }
 
-func SliceToSortedSet[T constraints.Ordered](s []T) SortedSet[T] {
-	var set SortedSet[T]
-	set.AddAll(s)
-	return set
+func NewSortedSetFromSlice[T constraints.Ordered](slice []T) SortedSet[T] {
+	set := make(SortedSet[T], len(slice))
+	copy(set, slice)
+	slices.Sort(set)
+	return slices.Compact(set)
 }
